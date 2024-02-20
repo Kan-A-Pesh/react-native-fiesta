36c06236d6eb93f93d7f7f1d593001c3ec6aeceb
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-02-20 21:01:27 +0100

    🔥 Delete expo example

--- expo-example/index.web.js
+++
@@ -1,7 +0,0 @@
-import { registerRootComponent } from 'expo';
-import { LoadSkiaWeb } from '@shopify/react-native-skia/lib/module/web';
-
-LoadSkiaWeb().then(async () => {
-  const App = (await import('./src/App')).default;
-  registerRootComponent(App);
-});
